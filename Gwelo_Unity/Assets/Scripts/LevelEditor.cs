dc3ada37a493f29ce14718393706f55884bffa1d
MauritusInvader <117859831+MauritusInvader@users.noreply.github.com>
2024-10-16 13:17:13 -0400

    26

--- Gwelo_Unity/Assets/Scripts/LevelEditor.cs
+++ Gwelo_Unity/Assets/Scripts/LevelEditor.cs
@@ -1,11 +1,12 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-// Karma's gonna come for all of us but I hope and I hope I just hope she comes, comes for you first OH
+// Karma's gonna come for all of us but I hope and I hope I just hope she comes, comes for you first
 // Joni's on a juice cleanse, joni's got the whole room trained
 // YOU WISH
 // YOU WERE
 // LIKE HER
+// Hold onto hope if you got it and don't let it go for nobody
 public class LevelEditor : MonoBehaviour
 {
     public GameObject[] prefabs; // Array of prefabs for placing
